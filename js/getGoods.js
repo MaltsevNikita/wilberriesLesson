61420d523b20bbb5b196a9a4d787d782da79ab9f
MaltsevNikita <mn121288@gmail.com>
2023-11-28 17:09:56 +0300

    Второй урок пройден

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,8 +1,26 @@
 const getGoods = () => {
-  fetch('https://glotest-7ccb9-default-rtdb.firebaseio.com/ds.json')
-    .then((res) => res.json())
-    .then(function (data) {
-      console.log(data);
+  const links = document.querySelectorAll('.navigation-link')
+
+  const getData = () => {
+    // fetch('/db/db.json')
+    fetch('https://glotest-7ccb9-default-rtdb.firebaseio.com/ds.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        localStorage.setItem('goods', JSON.stringify(data))
+
+      })
+  }
+
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault()
+      getData()
     })
+  })
+
+
+  // console.log(JSON.parse(localStorage.getItem('goods')))
 }
+
 getGoods()
