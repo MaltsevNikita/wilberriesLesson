28ecd31302c3fe1a06569357be1a09efec2a168e
MaltsevNikita <mn121288@gmail.com>
2023-11-29 14:34:34 +0300

    Коммит перед реализацией поиска

--- js/search.js
+++ js/search.js
@@ -2,9 +2,13 @@ const search = function () {
   const input = document.querySelector('.search-block > input')
   const inputBtn = document.querySelector('.search-block > button')
 
-  input.addEventListener('input', (event) => {
-    console.log(event.target.value);
-  })
+  try {
+    input.addEventListener('input', (event) => {
+      console.log(event.target.value);
+    })
+  } catch (e) {
+    console.error(e.message);
+  }
 }
 
 search()
