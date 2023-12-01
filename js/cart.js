5f9114edcae0d98c64514f97703429b3151d7d6c
MaltsevNikita <mn121288@gmail.com>
2023-12-01 10:56:16 +0300

    Промежуточный коммит. Ковыряюсь с добавлением\удалением товара из корзины

--- js/cart.js
+++ js/cart.js
@@ -5,15 +5,13 @@ const cart = function (params) {
   const goodsContainer = document.querySelector('.long-goods-list')
   const cartTable = document.querySelector('.cart-table__goods')
 
-  console.log(cartTable);
-
   const addToCart = (id) => {
     const goods = JSON.parse(localStorage.getItem('goods'))
     const clickedGood = goods.find(good => good.id === id)
     const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
 
     if (cart.some(good => good.id === clickedGood.id)) {
-      console.log('up');
+
       cart.map(good => {
         if (good.id === clickedGood.id) {
           good.count++
@@ -28,14 +26,73 @@ const cart = function (params) {
     localStorage.setItem('cart', JSON.stringify(cart))
   }
 
+  const deleteCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem('cart'))
 
-  cartBtn.addEventListener('click', () => {
+    const newCart = cart.filter(good => {
+      return good.id != id
+    })
+    localStorage.setItem('cart', JSON.stringify(newCart))
+    renderCartGoods(JSON.parse(localStorage.getItem('cart')))
+  }
+  const plusCartItem = (id) => {
+    const cart = localStorage.getItem('cart')
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }
+  const minusCartItem = (id) => {
+    const cart = localStorage.getItem('cart')
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }
+
+  const renderCartGoods = (goods) => {
+    cartTable.innerHTML = ''
+    goods.forEach(good => {
+      const tr = document.createElement('tr')
+      tr.innerHTML = `
+        <td>${good.name}</td>
+        <td>${good.price}$</td>
+        <td><button class="cart-btn-minus"">-</button></td>
+        <td>${good.count}</td>
+        <td><button class=" cart-btn-plus"">+</button></td>
+        <td>${+good.price * +good.count}</td>
+        <td><button class="cart-btn-delete"">x</button></td>
+      `
+
+      cartTable.append(tr)
+
+      tr.addEventListener('click', (e) => {
+        console.log(e.target);
+        if (e.target.classList.contains('cart-btn-minus')) {
+          console.log('minus');
+        } else if (e.target.classList.contains('cart-btn-plus')) {
+          console.log('plus');
+        } else if (e.target.classList.contains('cart-btn-delete')) {
+          deleteCartItem(good.id)
+        }
+
+      })
+    });
+  }
+
+  cartBtn.addEventListener('click', (event) => {
+    const cartArray = localStorage.getItem('cart') ?
+      JSON.parse(localStorage.getItem('cart')) : []
     cart.style.display = 'flex'
-    console.log('render goods');
+    renderCartGoods(cartArray)
 
   })
 
-  closeBtn.addEventListener('click', () => cart.style.display = '')
+  closeBtn.addEventListener('click', (event) => {
+    cart.style.display = ''
+  })
+
+  cart.addEventListener('click', (event) => {
+    if (!event.target.closest('.modal') && event.target.classList.contains('overlay')) {
+      cart.style.display = ''
+    }
+
+  }
+  )
 
 
   if (goodsContainer) {
